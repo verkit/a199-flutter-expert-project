7f8390134d4e1e88011e9b135fe80f9de6d59f2a
Verdy Bangkit <verdybangkit@gmail.com>
2021-11-27 15:49:12 +0700

    fix ssl pinning

--- core/lib/utils/ssl_pinning.dart
+++ core/lib/utils/ssl_pinning.dart
@@ -3,20 +3,24 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 
 class SSLClient {
   final http.Client client;
   SSLClient({required this.client});
 
   Future<SecurityContext> get globalContext async {
-    final sslCert = await rootBundle.load('certificates/themoviedb.pem');
+    final sslCert = await rootBundle.load('certificates/themoviedb.org.pem');
     SecurityContext securityContext = SecurityContext(withTrustedRoots: false);
     securityContext.setTrustedCertificatesBytes(sslCert.buffer.asInt8List());
     return securityContext;
   }
 
   Future<Response> get(Uri url, {Map<String, String>? headers}) async {
-    Response response = await client.get(url, headers: headers);
+    HttpClient client = HttpClient(context: await globalContext);
+    client.badCertificateCallback = (X509Certificate cert, String host, int port) => false;
+    IOClient ioClient = IOClient(client);
+    final response = await ioClient.get(url, headers: headers);
     return response;
   }
 }
